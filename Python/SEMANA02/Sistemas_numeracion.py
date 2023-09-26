a6c237d6486d62d3b4813927a7dc77e54caa5f2f
lulyrp <112672568+lulyrp@users.noreply.github.com>
2023-09-25 22:25:38 -0300

    Añadimos la segunda parte conversión
     Seguimos trabajando con: Decimal, Binario, Octal y Hexadecimal

--- Python/SEMANA02/Sistemas_numeracion.py
+++ Python/SEMANA02/Sistemas_numeracion.py
@@ -15,3 +15,24 @@ print(f"a octal: {a}")
 # SISTEMA HEXADECIMAL
 a = 0xA
 print(f"a hexadecimal: {a}")
+
+
+# BASE DECIMAL
+a = int('23', 10)
+print(f"a = base decimal: {a}")
+
+# BASE BINARIO
+a = int('10111', 2)
+print(f"a = base binario: {a}")
+
+# BASE OCTAL
+a = int('27', 8)
+print(f"a = base octal: {a}")
+
+# BASE HEXADECIMAL
+a = int('17', 16)
+print(f"a = base hexadecimal: {a}")
+
+# Base 5 sus valores son de 0 a 4
+a = int('344', 5)
+print(f"a = Base 5: {a}")
