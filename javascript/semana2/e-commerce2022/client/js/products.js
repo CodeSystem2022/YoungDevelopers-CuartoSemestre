cdd824f74dd09068a511b3763a772205b7de5731
valemorbi <113058574+valemorbi@users.noreply.github.com>
2023-09-27 22:28:02 -0300

    Subimos la 2da parte de la Semana 2 de JavaScript

---
+++ javascript/semana2/e-commerce2022/client/js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Cheddar Simple",
+        price: 1000,
+        quanty: 1,
+        img: "/client/media/cheddar simple.jpg"
+    },
+    {
+        id: 2,
+        productName: "Cuatro Medallones",
+        price: 2000,
+        quanty: 1,
+        img: "/client/media/cuatro medallon.jpg"
+    },
+    {
+        id: 3,
+        productName: "Varias",
+        price: 400,
+        quanty: 1,
+        img: "/client/media/varias.jpg"
+    },
+    {
+        id: 4,
+        productName: "Doble Medallones",
+        price: 1250,
+        quanty: 1,
+        img: "/client/media/doble medallon.jpg"
+    },
+    {
+        id: 5,
+        productName: "Triple Medallones",
+        price: 1500,
+        quanty: 1,
+        img: "/client/media/triple medallon.png"
+    }
+]
