4a358940b59bfcfb21ab6c15cc5730a5ed57e3d9
Micaela <72576818+micaela2020-01@users.noreply.github.com>
2023-10-09 20:34:30 -0300

    semana3 node

---
+++ node/semana3/app3.js
@@ -0,0 +1,12 @@
+console.log('Inicio del programa');
+setTimeout(() => {
+    console.log('Primer Timeout');
+}, 3000);
+setTimeout(() => {
+    console.log('Segundo Timeout');
+}, 0);
+setTimeout(() => {
+    console.log('Tercer Timeout');
+}, 0);
+
+console.log('Fin del programa');
