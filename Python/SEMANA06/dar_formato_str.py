9405f62d648f2af2018d2d76865ae2a08607120a
lurebolledo <112673733+lurebolledo@users.noreply.github.com>
2023-10-26 17:51:12 -0300

    Uso del metodo format (parte1)

--- Python/SEMANA06/dar_formato_str.py
+++ Python/SEMANA06/dar_formato_str.py
@@ -10,3 +10,12 @@ print(mensaje_con_formato)
 persona = ('Carla', 'Gomez', 5000.00)
 mensaje_con_formato = 'Hola %s %s . Tu sueldo es %.2f' # % persona # Aqui le pasamos el objeto que es la tupla
 print(mensaje_con_formato % persona)
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+# mensaje = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje_con_formato)
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
