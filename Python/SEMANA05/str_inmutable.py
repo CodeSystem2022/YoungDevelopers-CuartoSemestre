2696a16d013536ac2fcfcda700237a9ffc8f5bd2
Lucas Montoya <111904619+LucasM2003@users.noreply.github.com>
2023-10-25 22:22:16 -0300

    Añado 5.2 Dunder doc (__doc__)  y 5.3 String inmutables

---
+++ Python/SEMANA05/str_inmutable.py
@@ -0,0 +1,10 @@
+
+help(str.capitalize)
+mensaje1 = 'Hola Mundo'
+mensaje2 = mensaje1.capitalize
+
+print(f'Mensaje1: {mensaje1}, id: {id(mensaje1)}')
+print(f'Mensaje2: {mensaje2}, id: {id(mensaje2)}')
+
+mensaje1 += 'Adios'
+print(f'Mensaje1: {mensaje1}, id: {id(mensaje1)}')
