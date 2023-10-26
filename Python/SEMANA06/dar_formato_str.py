5686b8aa5a2c9d848f730410ee9c467754ae51a4
Lourdes Rebolledo <112672568+lulyrp@users.noreply.github.com>
2023-10-26 17:57:03 -0300

    Metodo format (parte 2 y 3)

--- Python/SEMANA06/dar_formato_str.py
+++ Python/SEMANA06/dar_formato_str.py
@@ -17,5 +17,13 @@ sueldo = 3000
 # mensaje = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
 # print(mensaje_con_formato)
 
-mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+# mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje)
+
+# mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad, s=sueldo)
+# print(mensaje)
+
+diccionario = {'nombre': 'Ivan', 'edad': 35, 'sueldo': 8000}
+mensaje = 'Nombre {dic[nombre]} Edad {dic[edad]} Sueldo {dic[sueldo]:.2f}'.format(dic=diccionario)
 print(mensaje)
+
