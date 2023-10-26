f1dd755dd17602bb88f911d89ef5f762ad6a19dc
Lucas Montoya <111904619+LucasM2003@users.noreply.github.com>
2023-10-25 22:49:52 -0300

    Añado la Primera parte de la semana 5 de python (#15)

---
+++ Python/SEMANA05/mi_clase.py
@@ -0,0 +1,22 @@
+
+class MiClase:
+    '''
+    Este es un ejemplo de la documentacion
+    de esta clase.
+    Es solo para saber que aqui esta MiClase
+    '''
+    def __init__(self):
+        '''
+        Aqui esta el metodo dunder init
+        Este metodo es el de Inicio
+        para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el metodo creado por mi que recibe parametros
+        Detecto los parametros automaticamente
+        :param param1: Este es el parametro numero 1
+        :param param2: Este es el parametro numero 2
+        :return: Este es el valor de retorno
+        '''
