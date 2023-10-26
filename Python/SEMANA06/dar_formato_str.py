ed9006ac7527ecf125d4929a4cd8cef9b7bb8f4c
lurebolledo <112673733+lurebolledo@users.noreply.github.com>
2023-10-26 17:48:32 -0300

    Creamos una tupla

--- Python/SEMANA06/dar_formato_str.py
+++ Python/SEMANA06/dar_formato_str.py
@@ -5,3 +5,8 @@ nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años'% (nombre, edad)
 print(mensaje_con_formato)
+
+# Creamos una tupla
+persona = ('Carla', 'Gomez', 5000.00)
+mensaje_con_formato = 'Hola %s %s . Tu sueldo es %.2f' # % persona # Aqui le pasamos el objeto que es la tupla
+print(mensaje_con_formato % persona)
