0ef4e84278349298d0caf6f92de8b25a03979d95
Micaela <72576818+micaela2020-01@users.noreply.github.com>
2023-11-06 16:11:25 -0300

    Fundamentos/callback

---
+++ node/semana8/callbacks.js
@@ -0,0 +1,25 @@
+function hola(nombre, miCallback){
+  setTimeout(function(){
+    //console.log('Hola,soy una funcion asincrona');
+    console.log('Hola'+nombre);
+    miCallback(nombre);
+  },1000);
+}
+
+function adios(nombre,otroCallback){
+    setTimeout(function(){
+        console.log('Adios',nombre);
+        otroCallback();
+    },1500);
+}
+
+console.log('Iniciando el proceso ...');
+/*hola('Carlos',function(nombre){
+    adios(nombre,function(){
+        console.log('terminando el proceso...');
+    });
+});
+*/
+
+//hola('Carlos',function(){});
+//adios('Carlos',function(){});
