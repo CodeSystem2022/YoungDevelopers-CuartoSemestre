475a3451fe5669bbadf09de47e4a662a4af0433d
Valentín Morbidelli <113058574+valemorbi@users.noreply.github.com>
2023-11-08 11:30:18 -0300

    Subimos la primera parte de promesas

---
+++ node/Semana10/promises.js
@@ -0,0 +1,20 @@
+function hola(nombre){
+    return new Promise(function (resolve, reject){
+        setTimeout( function () {
+            console.log('Hola '+ nombre);
+            resolve(nombre);
+        }, 1000);
+    })
+    
+}
+
+function hablar(nombre){
+    return new Promise((resolve, reject) => { //usamos la sintaxis ES6
+            setTimeout(function() {
+            console.log('bla bla bla bla');
+            resolve(nombre);
+        }, 1000);
+    })
+    
+}
+
