61590512951704584c01b58981ef2449364b3042
Valentín Morbidelli <113058574+valemorbi@users.noreply.github.com>
2023-11-10 18:03:34 -0300

    Subo la 2da Parte de la Clase 13 Módulos -> File System

--- node/semana13/modulos/fs.js
+++ node/semana13/modulos/fs.js
@@ -12,4 +12,18 @@ function escribir(ruta, contenido) {
     fs.writeFile(ruta, contenido, function(err){
         if(err){
             console.log('No se ha podido escribir', err);
-        } 
+        } } else {
+            console.log('Se ha escrito correctamente');
+        }
+    })
+}
+
+//Tercero eliminamos el archivo1.txt
+function borrar(ruta,cb){
+    fs.unlink(ruta, cb); //elimina de manera asincrona
+}
+
+borrar(`${__dirname}/archivo1.txt`, console.log);
+//escribir(`${__dirname}/archivo1.txt`, 'Reescribimos el archivo', console.log);
+//leer(`${__dirname}/archivo1.txt`, console.log); //Sintaxis ES6
+        
